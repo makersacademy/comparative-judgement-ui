2a14e6dc49178bc881bcc1424e445b9ca239113b
Alex <alex@scotttonge.com>
2018-03-13 14:59:04 +0000

    refactored into two tests for updating state

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,17 @@ import App from './App';
 import { mount } from 'enzyme';
 
 describe('App', () => {
-  it('sets the image URL', () => {
+  it('loads with no image', () => {
     const wrapper = mount(<App />);
     const inst = wrapper.instance()
+
+    expect(inst.state.imageUrl).toEqual('')
+  })
+
+  it('updates state with image url', () => {
+    const wrapper = mount(<App />);
+    const inst = wrapper.instance()
+
     inst.setImage()
     expect(inst.state.imageUrl).toEqual('./jester.jpeg');
   })
