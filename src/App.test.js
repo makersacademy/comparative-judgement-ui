f016c8927cf7bed9e54b25fad5a0564691b8a84c
Alex <alex@scotttonge.com>
2018-03-13 15:02:13 +0000

    refactored tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,17 +3,14 @@ import App from './App';
 import { mount } from 'enzyme';
 
 describe('App', () => {
-  it('loads with no image', () => {
-    const wrapper = mount(<App />);
-    const inst = wrapper.instance()
+  const wrapper = mount(<App />);
+  const inst = wrapper.instance()
 
+  it('loads with no image', () => {
     expect(inst.state.imageUrl).toEqual('')
   })
 
   it('updates state with image url', () => {
-    const wrapper = mount(<App />);
-    const inst = wrapper.instance()
-
     inst.setImage()
     expect(inst.state.imageUrl).toEqual('./jester.jpeg');
   })
