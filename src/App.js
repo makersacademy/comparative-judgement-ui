67801dac483c5129b3e030f60c52487a452ba6ee
oliverpople <oliverpople@gmail.com>
2018-03-15 11:52:59 +0000

    Added mockPOSTData (may be required for later tests).  Also, began second fetch API test, but currently enable to call mock funciton

--- src/App.js
+++ src/App.js
@@ -4,26 +4,39 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state =  { imageURL: [] }
+    this.state =  { imageURLs:  [] }
   }
 
   componentDidMount() {
     fetch('/comparison')
       .then(res => res.json())
       .then(images => this.setState({
-         imageURL: [...this.state.imageURL, images]
-
+         imageURLs: [...this.state.imageURLs, images]
       }))
       // .bind(this);
       console.log(this.state)
   }
 
+  submitChoice(data) {
+    fetch('/comparison', {
+      method: 'POST',
+      body: JSON.stringify([...this.state.imageURLs, data]),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
+  }
+
+
   render() {
 
     return (
       <div className="App">
-        <img id='firstImage' className='image' src={this.state.imageURL} />
-        <img id='secondImage' className='image' src={this.state.imageURL} />
+        <img id='firstImage' className='image' src={this.state.imageURLs[0]} />
+        <img id='secondImage' className='image' src={this.state.imageURLs[1]} />
+        <button id='buttonOption1' className='button' onClick={this.submitChoice(this.state.imageURLs[0])}>Select Option 1</button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -2,28 +2,42 @@ import React from 'react';
 import App from './App';
 import { mount, shallow } from 'enzyme';
 import { waitForState } from 'enzyme-async-helpers';
-
+//
 describe('App', () => {
-  it('it fetches an image URL on mount and sets it to state',  async () => {
+  it('it fetches image URLs on mount and sets them to state',  async () => {
   //arrange
     const fs = require("fs");
-    const mockData = fs.readFileSync('./public/mockData.json');
-    console.log(mockData)
+    const mockGETData = fs.readFileSync('./public/mockGETData.json');
 
     global.fetch = jest.fn(async () => ({
       ok: true,
       status: 200,
       json: async () => ({
-        mockData
+        mockGETData
       })
     }))
 
     const app = mount(<App />);
 
   //act
-    await waitForState(app, state => state.imageURL.length > 0)
+    await waitForState(app, state => state.imageURLs.length > 0)
 
   //assert
-    expect(app.instance().state.imageURL.length).toBe([mockData].length)
+    expect(app.instance().state.imageURLs.length).toBe([mockGETData].length)
   })
+
+  it('should call the submitChoice method on click' , () => {
+
+  //arrange
+    const mockSubmitChoice = jest.fn();
+    const app = mount(<App submitChoice={ mockSubmitChoice } />);
+
+  //act
+    app.find('#buttonOption1').simulate('click');
+
+  //assert
+    expect(mockSubmitChoice).toHaveBeenCalledTimes(1);
+
+  })
+
 })
